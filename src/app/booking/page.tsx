c4851fafeb31f6e646f00c0b692d07bd2428cc60
danieltolmon <daniel.toledomonfort@gmail.com>
2024-10-04 12:10:13 +0200

    add booking page tidycal

---
+++ src/app/booking/page.tsx
@@ -0,0 +1,28 @@
+import React, { FunctionComponent } from 'react'
+import Script from 'next/script'
+import Link from 'next/link';
+
+type Props = {
+  type: 'free' | 'online' | 'presential'
+}
+
+
+const Bookings: FunctionComponent<Props> = ({ type }) => {
+
+  return (
+    <>
+      <section id="booking" className="py-16 md:py-20 lg:py-28 bg-light mt-16">
+        <div className='container border border-solid border-black'>
+
+          <div className="tidycal-embed" data-path="danielawakenscarlet/15-minute-meeting" />
+          <Script src="https://asset-tidycal.b-cdn.net/js/embed.js" async />
+
+
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Bookings;
+
