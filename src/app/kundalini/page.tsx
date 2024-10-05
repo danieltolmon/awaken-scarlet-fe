fdac1d60a61de37f3e1c69844f11d5208a08cac4
danieltolmon <daniel.toledomonfort@gmail.com>
2024-10-05 13:29:42 +0200

    add video hero

--- src/app/kundalini/page.tsx
+++ src/app/kundalini/page.tsx
@@ -1,6 +1,7 @@
 // import Kundalini from "@/components/Kundalini";
 
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Kundalini | Scarlet Kundalini Activation",
@@ -11,12 +12,24 @@ export const metadata: Metadata = {
 const KundaliniPage = () => {
   return (
     <div className="mt-60 h-80">
-      <h1 className=" text-black animate-pulse mb-4 text-center text-3xl font-bold !leading-tight dark:text-white sm:text-4xl md:text-[45px]">
+      <h1 className="font-luto text-black animate-pulse mb-4 text-center text-3xl font-bold !leading-tight dark:text-white sm:text-4xl md:text-[45px]">
         Coming Soon
       </h1>
-      <p className="text-black text-lg mb-8 text-center">
-        We&apos;re working hard to bring you something amazing. Stay tuned!
+      <p className="font-quicksand text-black text-lg mb-8 text-center">
+        We&apos;re working so hard to explain what is kundalini activation in detail. <strong>Stay tuned!</strong>
+        <br />
+        For now, if you want to know more, you can:
       </p>
+
+      <div className="flex gap-4 justify-center">
+
+        <Link href='/contact' className="font-quicksand py-2 px-5 border border-solid border-primary rounded-md gap-2 text-md text-black font-semibold flex items-center justify-between transition-all duration-500 hover:bg-white/5">
+          Contact me
+        </Link>
+        <Link href='https://tidycal.com/danielawakenscarlet/free-call' target='_blank' className="font-quicksand bg-primary py-2 px-5 border border-solid border-primary rounded-md gap-2 text-md text-white font-semibold flex items-center justify-between transition-all duration-500 hover:bg-primary/80">
+          Book free call
+        </Link>
+      </div>
     </div>
   );
 };
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -6,46 +6,50 @@ const Hero = () => {
     <>
       <section
         id="home"
-        className="relative min-h-screen flex items-center z-10 overflow-hidden bg-light pb-16 pt-[120px] dark:bg-gray-dark md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
+        className="relative  h-screen flex items-center z-10 overflow-hidden bg-light pb-16 pt-[150px] dark:bg-gray-dark md:pb-[120px] md:pt-[133px] xl:pb-[160px] xl:pt-[200px] 2xl:pb-[200px] 2xl:pt-[210px]"
       >
+
+
         <div className="container items-center">
           <div className="-mx-4 flex flex-wrap items-center">
             <div className="w-full px-4">
-              <div className="mx-auto max-w-[800px] text-left">
-                <h1 className="font-luto mb-2 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+              <div className=" max-w-[800px] text-left">
+                <h1 className="font-luto mb-12 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
                   Kundalini Activations
                 </h1>
-                <p className="font-quicksand mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
+                {/* <p className="font-quicksand mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
                   Awaken Your Inner Life Force Energy and Transform Your Self
-                </p>
+                </p> */}
                 <div className="flex flex-col items-center justify-start space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-                  <Link
-                    href="/sessions"
-                    className="font-quicksand rounded-md bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
-                  >
-                    Book your Session
-                  </Link>
+                 
                   <Link
                     href="/kundalini"
                     className="font-quicksand inline-block rounded-md bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
                     What is Kundalini Activation ?
                   </Link>
+                  <Link
+                    href="/sessions"
+                    className="font-quicksand rounded-md bg-light px-8 py-4 text-base font-semibold text-primary duration-300 ease-in-out hover:bg-light/80"
+                  >
+                    Book your Session
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="absolute right-0 bottom-0 z-[-1] opacity-30 md:opacity-100">
-          <Image
-            src="/images/hero/hero.webp"
-            alt="logo"
-            width={350}
-            height={350}
-            className="max-w-full dark:hidden"
-          />
-        </div>
 
+
+
+
+        <div className="absolute top-1/2 transform -translate-y-1/2 right-0 z-[-1] opacity-50">
+
+          <video className="h-screen lg:h-[1200px] w-screen object-cover object-right" autoPlay loop muted playsInline controls={false}>
+            <source src="/images/hero/1005.mp4" type="video/mp4" />
+            video not sopported.
+          </video>
+        </div>
       </section>
     </>
   );
