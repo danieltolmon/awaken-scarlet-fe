44da62eac665570de43a13a8c6ea5e1f6ed11dce
danieltolmon <daniel.toledomonfort@gmail.com>
2024-10-05 22:42:13 +0200

    fix production issue

--- src/components/Daniel/index.tsx
+++ src/components/Daniel/index.tsx
@@ -18,7 +18,7 @@ const Daniel = () => {
                 <p className="font-quicksand text-base text-justify !leading-relaxed text-body-color md:text-lg">
                   Living between India and Barcelona, Daniel embodies a spiritual way of life, weaving the wisdom of the East into the modern world. His journey unites ancient traditions with contemporary knowledge, merging mind and body, spirituality and science.
                   <br /> <br />
-                  Daniel's path began in renewable energy engineering and software development, where he spent several years creating solutions for the physical world. However, life chose a different direction for him—one dedicated to elevating human energy and dismantling limiting belief systems. His mission is to help people transform their perception of life and connecting them with their higher selves.
+                  Daniel&apos;s path began in renewable energy engineering and software development, where he spent several years creating solutions for the physical world. However, life chose a different direction for him—one dedicated to elevating human energy and dismantling limiting belief systems. His mission is to help people transform their perception of life and connecting them with their higher selves.
                   <br /> <br />
                   Through the practices of yoga, meditation, and tantra, Daniel has developed a heightened sensitivity to energy, allowing him to guide and empower individuals toward higher states of consciousness. He nurtures a collective shift towards a more aware and connected humanity, envisioning a future where spiritual awakening becomes the foundation for how we live, love, and grow as humanity.
                   <br /> <br />
