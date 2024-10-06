3c50ce7fa23debfbfa48c896a9142dce9066004e
danieltolmon <daniel.toledomonfort@gmail.com>
2024-10-06 09:42:54 +0200

    change daniel text

--- src/components/Daniel/index.tsx
+++ src/components/Daniel/index.tsx
@@ -20,7 +20,7 @@ const Daniel = () => {
                   <br /> <br />
                   Daniel&apos;s path began in renewable energy engineering and software development, where he spent several years creating solutions for the physical world. However, life chose a different direction for him—one dedicated to elevating human energy and dismantling limiting belief systems. His mission is to help people transform their perception of life and connecting them with their higher selves.
                   <br /> <br />
-                  Through the practices of yoga, meditation, and tantra, Daniel has developed a heightened sensitivity to energy, allowing him to guide and empower individuals toward higher states of consciousness. He nurtures a collective shift towards a more aware and connected humanity, envisioning a future where spiritual awakening becomes the foundation for how we live, love, and grow as humanity.
+                  Through the practices of yoga, meditation, and tantra, Daniel has developed a heightened sensitivity to energy, allowing him to guide and empower individuals toward higher states of consciousness. He nurtures a collective shift towards a more aware and connected humanity, envisioning a future where spiritual awakening becomes the foundation for how we live, love, and evolve.
                   <br /> <br />
                   Join Daniel on this journey towards the new era of spiritual awakening, where duality dissolves and we reunite with the divine.
                 </p>
