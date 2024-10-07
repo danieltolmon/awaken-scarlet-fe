6cd6e5b627560e64b7535ebf4ca3184d312b9e4b
danieltolmon <daniel.toledomonfort@gmail.com>
2024-10-07 09:25:28 +0200

    fix quote misspeling

--- src/components/Quote/index.tsx
+++ src/components/Quote/index.tsx
@@ -6,8 +6,8 @@ const Quote = () => {
         <div className="container">
           <h2 className="font-quicksand mb-4 text-2xl sm:text-4xl md:text-[45px] font-bold !leading-tight text-center text-light dark:text-white ">
             When consciousness, <span className="bold italic font-quicksand">Shiva</span>,
-            <br />Witnesses and worships devine energy, <span className="bold italic font-quicksand">Shakti</span>,
-            <br />Kundalini awakens, healing and dancing
+            <br />witnesses and worships Divine energy, <span className="bold italic font-quicksand">Shakti</span>,
+            <br />kundalini awakens, healing and dancing
             <br />
             <span className="font-brittany text-xl sm:text-3xl md:text-[38px]">- Daniel</span>
           </h2>
